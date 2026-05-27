fb4c1dc4bb7d1909be3e33b49b51e2b56a099f3f
Parambrata Das <parambratadas7@gmail.com>
2026-05-27 19:27:27 +0530

    Update core c file.c

--- core c file.c
+++ core c file.c
@@ -1,39 +1,78 @@
 #include <stdio.h>
-void main ()
+#include <stdlib.h>
+int main ()
 {
- int ch,a,b;
+ int i,n;
+ char ch;
+ int total;
  '(char +,-,*,/,!)';
- printf("Enter the first number: "); 
- scanf("%d",&a);
- printf("Enter the second number: ");
- scanf("%d",&b);
- do{
-
+ printf("Enter the number of elements you want to calculate: ");
+ scanf("%d",&n);
+ int num[n];
+ 
+ for(i=0;i<n;i++)
+   {
+       printf("\nEnter the numbers: "); 
+       scanf("%d",&num[i]);
+   }
+       printf("\nNOTE: YOU CAN PERFORM ONE OPERATION AT A TIME; UPDATE COMING SOON !!!");
        printf("\nEnter + for addition\nEnter - for subtraction\nEnter * for multiplication\nEnter / for Division\nEnter ! for exit\n");
+ do{       
        printf("\nEnter the operation: ");
-       scanf("%s",&ch);
+       scanf(" %c",&ch);     
+       if(ch=='+')
+       {
+       	total=0;
+	   }
+	   else if(ch=='*')
+	   {
+	   	total=1;
+	   }
+	   else
+	   {
+	   	total=num[0];
+	   }
        switch (ch)
     {
         case '+':
-        ch=a+b;
-        printf("\n%d\n",ch);
+       	for(i=0;i<n;i++)
+		{
+           total+=num[i];
+		}
+        printf("\n%d\n",total);
         break;
         case '-':
-        ch=a-b;
-        printf("\n%d\n",ch);
+        for(i=1;i<n;i++)
+		{
+           total-=num[i];
+		}
+        printf("\n%d\n",total);
         break;
         case '*':
-        ch=a*b;
-        printf("\n%d\n",ch);
+        for(i=0;i<n;i++)
+		{
+           total*=num[i];
+		}
+        printf("\n%d\n",total);
         break;
         case '/':
-        ch=a/b;
-        printf("\n%d\n",ch);
+        for(i=1;i<n;i++)
+		{
+			if(num[i]==0)
+			{
+				printf("!!INVALID DIVISION!!");
+				return 1;
+			}
+            total/=num[i];
+		}
+        printf("\n%d\n",total);
+        printf("NOTICE: THE OUTPUT ONLY COMES IN INTEGERS; UPDATE COMING SOON\n");
         break;
         case '!':
         exit (0);
         default:
-        printf("\nEnter the correct option\n");
+        printf("\nINVALID CHOICE!!!\n");
     }
- }while (1);  
+ }while (1); 
+ return 0; 
 }        
