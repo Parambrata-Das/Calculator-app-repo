b1300c96750b8a7c3730d376f265ab968d9b89df
Parambrata Das <parambratadas7@gmail.com>
2026-05-28 19:32:23 +0530

    Update core c file.c

--- core c file.c
+++ core c file.c
@@ -3,18 +3,28 @@
 int main ()
 {
  int i,n;
- char ch;
+ char ch,junk;
  int total;
- '(char +,-,*,/,!)';
- printf("Enter the number of elements you want to calculate: ");
- scanf("%d",&n);
- int num[n];
+ '(char +,-,*,/,!,@)';
+ printf("NOTE 1: Enter the number one by one don't overpress!!!\n");
+ printf("NOTE 2: You can only calculate upto 100 numbers.\n");
+ printf("NOTE 3: Enter '@' when you finished typing all the numbers.\n");
+ int num[100];
  
- for(i=0;i<n;i++)
+ for(n=0;n<100;n++)
    {
        printf("\nEnter the numbers: "); 
-       scanf("%d",&num[i]);
-   }
+       if( scanf("%d",&num[n])!=1)
+       {
+       	    break;
+	   }
+    }
+	   if(n==0)
+	   {
+	   	    printf("No number entered!!!\n");
+	   	    return 1;
+	   }
+       scanf(" %c",&junk);
        printf("\nNOTE: YOU CAN PERFORM ONE OPERATION AT A TIME; UPDATE COMING SOON !!!");
        printf("\nEnter + for addition\nEnter - for subtraction\nEnter * for multiplication\nEnter / for Division\nEnter ! for exit\n");
  do{       
@@ -61,7 +71,7 @@ int main ()
 			if(num[i]==0)
 			{
 				printf("!!INVALID DIVISION!!");
-				return 1;
+				break;
 			}
             total/=num[i];
 		}
